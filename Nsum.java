8d809126c5c9eea66d68a8ec81b44b971edfd47c
andriilguvi <andriilguvi68@gmail.com>
2017-07-17 11:13:24 +0530

    Create Nsum.java

---
+++ Nsum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Nsum
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int in=5;
+int sum=0;
+for(int i=0;i<=in;i++)
+{
+sum=sum+i;
+System.out.print(sum);
+}
+}
+  }
